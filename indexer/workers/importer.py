fb051e526dc3f6ee6f7330a69851334d3f779be8
=xavierfrank4 <=>
2023-08-02 14:49:49 +0300

    index mapping

--- indexer/workers/importer.py
+++ indexer/workers/importer.py
@@ -34,7 +34,7 @@ class ElasticsearchConnector:
                     "normalized_url": {"type": "keyword"},
                     "canonical_domain": {"type": "keyword"},
                     "publication_date": {"type": "date"},
-                    "language": {"type": "keyword", "fielddata": True},
+                    "language": {"type": "text", "fielddata": True},
                     "full_language": {"type": "keyword"},
                     "text_extraction": {"type": "keyword"},
                     "article_title": {"type": "text", "fielddata": True},
